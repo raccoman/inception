d24e4354a2466ce6801e3908cc7ff02ed2e46ac3
Francesca Minardi <fminardi@e3r11p7.42roma.it>
2022-01-16 17:56:13 +0100

    push

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -6,7 +6,7 @@ if [ ! -d "/run/mysqld" ]; then
 fi
 
 if [ -d /var/lib/mysql/mysql ]; then
-	echo '[i] MySQL directory already present, skipping creation'
+	echo "[i] MySQL directory already present, skipping creation"
 else
 	echo "[i] MySQL data directory not found, creating initial DBs"
 
@@ -15,42 +15,38 @@ else
 	# init database
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 	echo '[i] Database initialized'
-fi
 
-echo "[i] MySql root password: $MYSQL_ROOT_PWD"
+	echo "[i] MySql root password: $MYSQL_ROOT_PWD"
 
-tfile=`mktemp`
-if [ ! -f "$tfile" ]; then
-	return 1
-fi
+	tfile=`mktemp`
+	if [ ! -f "$tfile" ]; then
+		return 1
+	fi
 
-cat << EOF > $tfile
+	cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD' WITH GRANT OPTION;
 EOF
 
-# MySQL queries
-# echo "[i] Updating root password"
-# echo "USE mysql;" >> $tfile
-# echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD' WITH GRANT OPTION;" >> $tfile
+	# MySQL queries
+	# echo "[i] Updating root password"
+	# echo "USE mysql;" >> $tfile
+	# echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD' WITH GRANT OPTION;" >> $tfile
 
-echo "[i] Creating database: $WP_DATABASE_NAME"
-echo "CREATE DATABASE IF NOT EXISTS \`$WP_DATABASE_NAME\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+	echo "[i] Creating database: $WP_DATABASE_NAME"
+	echo "CREATE DATABASE IF NOT EXISTS \`$WP_DATABASE_NAME\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
 
-echo "[i] Creating user: $WP_DATABASE_USR with password $WP_DATABASE_PWD"
-echo "GRANT ALL ON \`$WP_DATABASE_NAME\`.* to '$WP_DATABASE_USR'@'%' IDENTIFIED BY '$WP_DATABASE_PWD';" >> $tfile
+	echo "[i] Creating user: $WP_DATABASE_USR with password $WP_DATABASE_PWD"
+	echo "GRANT ALL ON \`$WP_DATABASE_NAME\`.* to '$WP_DATABASE_USR'@'%' IDENTIFIED BY '$WP_DATABASE_PWD';" >> $tfile
 
-echo "FLUSH PRIVILEGES;" >> $tfile
+	echo "FLUSH PRIVILEGES;" >> $tfile
 
-# run sql in tempfile
-echo "[i] Executing queries: $tfile"
-/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
-rm -f $tfile
-
-
-echo "[i] Sleeping 3 seconds..."
-sleep 3
+	# run sql in tempfile
+	echo "[i] Executing queries: $tfile"
+	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
+	rm -f $tfile
+fi
 
 echo "[i] Starting mariadb server"
 exec /usr/bin/mysqld --user=mysql --console
