1c18c00708d8950f190a594e91fe1870f3eb2025
Riccardo Accomando <riccardoaccoma@gmail.com>
2022-01-18 17:19:17 +0100

    Finished mandatory pt.3

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -7,13 +7,13 @@ done
 
 if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 
-    mv /tmp/wp-config.php /var/www/html/wordpress/wp-config.php
-
     wp core download
+    wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci"
     wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email
     wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD
     wp theme install inspiro --activate
     wp plugin update --all
+    
 fi
 
 echo "Wordpress listesting on :9000"
