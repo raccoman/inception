d0b559b457d066453c41b8aa2c62379bee9e62e9
Riccardo Accomando <riccardoaccoma@gmail.com>
2022-01-20 16:44:12 +0100

    Finished inception

---
+++ Makefile
@@ -0,0 +1,20 @@
+NAME = inception
+
+all: purge reload
+
+linux:
+	@ echo "127.0.0.1 raccoman.42.fr" >> /etc/hosts
+	
+stop:
+	@ docker-compose -f srcs/docker-compose.yml down
+
+clean: stop
+	@ rm -rf ~/Desktop/inception
+
+prune: clean
+	@ docker system prune -f
+
+reload: 
+	@ docker-compose -f srcs/docker-compose.yml up --build
+
+.PHONY: linux stop clean prune reload all
--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -5,16 +5,20 @@ while ! mariadb -h$MYSQL_HOST -u$WP_DATABASE_USR -p$WP_DATABASE_PWD $WP_DATABASE
     sleep 3
 done
 
-if [ ! -f "/var/www/html/index.php" ]; then
+if [ ! -f "/var/www/html/index.html" ]; then
 
-    mv /tmp/adminer.php /var/www/html/
-    mv /tmp/adminer.css /var/www/html/
+    # static website
+    mv /tmp/index.html /var/www/html/index.html
 
-    wp core download
-    wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci"
-    wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email
-    wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD
-    wp theme install inspiro --activate
+    # adminer
+    wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php &> /dev/null
+    wget https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css -O /var/www/html/adminer.css &> /dev/null
+
+    wp core download --allow-root
+    wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+    wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+    wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+    wp theme install inspiro --activate --allow-root
 
     # enable redis cache
     sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOST' );"      wp-config.php
@@ -24,12 +28,12 @@ if [ ! -f "/var/www/html/index.php" ]; then
     sed -i "43i define( 'WP_REDIS_READ_TIMEOUT', 1 );"          wp-config.php
     sed -i "44i define( 'WP_REDIS_DATABASE', 0 );\n"            wp-config.php
 
-    wp plugin install redis-cache --activate
-    wp plugin update --all
+    wp plugin install redis-cache --activate --allow-root
+    wp plugin update --all --allow-root
 
 fi
 
-wp redis enable
+wp redis enable --allow-root
 
 echo "Wordpress started on :9000"
 /usr/sbin/php-fpm7 -F -R
